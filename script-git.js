779f5920df9a44ba279e4f6359e66dd0c50dfb96
Petro <hanziypetro@gmail.com>
2024-06-01 17:12:49 +0300

    condition and cycle

--- script-git.js
+++ script-git.js
@@ -8,13 +8,29 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один із переглянутих  останніх фільмів,', '');
-const b = prompt('На скільки його оцінюєте?', '');
-const c = prompt('Один із переглянутих останніх фільмів?', '');
-const d = prompt('На скільки його оцінюєте?', '');
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один із переглянутих  останніх фільмів,', ''),
+         b = prompt('На скільки його оцінюєте?', '');  
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+   } else {
+      i--;
+   }
+};
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+   console.log('Подивились мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('Ви класний глядач');
+} else if (personalMovieDB.count >= 30) {
+   console.log('ви кіноман');
+} else {
+   console.log('Error');
+};
 
 
 console.log(personalMovieDB);
+
+
+
+
