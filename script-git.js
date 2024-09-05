d61089a0247a86455ce0abf3fb614f9d1a8b847f
Petro <hanziypetro@gmail.com>
2024-09-05 18:01:04 +0300

    setTimeout, setInterval

--- script-git.js
+++ script-git.js
@@ -529,3 +529,50 @@
 
 // console.log(bigint + BigInt(number));
 // console.log(Number(bigint) + number);
+
+// const timeId = setTimeout(function(text) {
+//    console.log(text);
+// }, 2000, 'Hello');
+
+
+const btn = document.querySelector('.btn');
+// let twoTime,
+//     i = 0;
+
+function myAnimation() {
+   const elem = document.querySelector('.box');
+   let pos = 0;
+   const id = setInterval(frame, 10);
+   function frame() {
+      if (pos == 300) {
+         clearInterval(id);
+      } else {
+         pos++
+         elem.style.top = pos + 'px';
+         elem.style.left = pos + 'px';
+      }
+   }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// btn.addEventListener('click', () => {
+//    // const twoTime = setTimeout(logger, 2000);
+//    twoTime = setInterval(logger, 500);
+  
+// });
+
+// function logger () {
+//    if (i === 3) {
+//       clearInterval(twoTime);
+//    }
+//    console.log('text');
+//    i++;
+// }
+
+
+
+ 
+
+
+
